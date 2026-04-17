707af8a7c16cdd84885e60ad75ea4fc656bf3505
Mary <git@mary.my.id>
2026-04-17 18:09:22 +0700

    chore: upgrade dependencies

--- src/lib/type-utils.ts
+++ src/lib/type-utils.ts
@@ -1,6 +1,7 @@
 export type Assert<T extends true> = T;
 
 export type IsEqual<A, B> =
+	// oxlint-disable-next-line typescript-eslint/no-unnecessary-type-parameters -- exact-equality trick requires generic functions to defer resolution
 	(<T>() => T extends A ? 1 : 2) extends <T>() => T extends B ? 1 : 2 ? true : false;
 
 export type Simplify<T> = { [K in keyof T]: T[K] } & {};
